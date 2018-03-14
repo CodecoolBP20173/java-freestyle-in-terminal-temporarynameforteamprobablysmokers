440ed97445173c617a3b2449925d7a1dacb8f6a5
Tamas Abraham <tomoabraham92@gmail.com>
2018-03-14 13:38:19 +0100

    Plays youtube audio

---
+++ src/com/codecool/app/Mediaplayer.java
@@ -0,0 +1,14 @@
+package com.codecool.app;
+
+import java.util.*;
+
+class Mediaplayer{
+	public static void main(String args[]){
+		try{
+			ProcessBuilder pb=new ProcessBuilder("cvlc", "--global-key-quit=s","--no-video", /*songData[0][1]*/ "https://www.youtube.com/watch?v=75LnzY7UnZE");
+    		 pb.start();
+		} catch (Exception e){
+			System.out.println(e);
+		}
+	}
+}
