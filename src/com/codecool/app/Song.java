31c2aacb2e1f010a700c29b60cf35d46297c3de6
Gergely Kovacs <gergelyk89@gmail.com>
2018-03-14 13:32:00 +0100

    added artist name to Song object

--- src/com/codecool/app/Song.java
+++ src/com/codecool/app/Song.java
@@ -6,26 +6,45 @@ import java.util.TreeMap;
 public class Song {
     private String title;
     private String  link;
+    private String  artist;
     public static Map<String,Song> objectList = new TreeMap<>();
 
-    public Song(String title, String link) {
+    public Song(String title, String link, String artist) {
         this.title = title;
         this.link = link;
+	this.artist = artist;
     }
 
     public String getTitle() {
         return title;
+    }
+	
+    public String getLink() {
+        return link;
+    }
+
+    public String getArtist() {
+        return artist;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getLink() {
-        return link;
-    }
-
     public void setLink(String link) {
         this.link = link;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String txtReadyString(String link, String title, String Artist) {
+	this.artist = artist;
+        this.link = link;
+	this.title = title;
+	stringToTxt = link + "," + title + "," + artist + ";";
+	return stringToTxt;
+	
     }
 }
